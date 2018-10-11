90107a5e67489f2a42f6c06e6525fd4bfcd5dce5
nilarrus <nilarrus69@gmail.com>
2018-10-11 19:01:30 +0200

    cosas de js

---
+++ ejemplos/script.js
@@ -0,0 +1,9 @@
+document.getElementById("buton").addEventlistener("click",funcion);
+//prototipo mayusculas funciones minusculas
+function functionName() {
+  this.algo;
+  this.algo;
+}
+ClassName.prototype.methodName = function () {
+  //code
+};
