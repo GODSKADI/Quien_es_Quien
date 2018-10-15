0adb55d0965ffb72f7027d649e3bd53eeff6e6ff
nilarrus <nilarrus69@gmail.com>
2018-10-15 20:24:18 +0200

    cosas de fitcheros

--- pruebas/index.php
+++ pruebas/index.php
@@ -6,15 +6,32 @@
   </head>
   <body>
     <?php
-    $path = "cosas.txt";
-    if(!file_exists($path)){
-      exit("File Not found");
+    //config
+    $config = "config_p.txt";
+    if(!file_exists($config)){
+      exit("File Config Not found");
     }
-
-    $lineas = file($path);
+    $cartes= array();
+    $lineas = file($config);
     foreach ($lineas as $num_linea => $linea) {
-      echo "<p>Linea {$num_linea} :".htmlspecialchars($linea)."</p>";
+      array_push($cartes,strrchr($linea," "));
     }
+    print_r($cartes);
+    //imagenes
+    $imag = "imagenes_p.txt";
+    if(!file_exists($imag)){
+      exit("File Imagenes Not found");
+    }
+    $imagenes = array();
+    $imagen= array();
+    $filas = file($imag);
+    foreach ($filas as $key => $value) {
+      array_push($imagen,$value);
+    }
+    print_r($imagen);
+    $entrada = "imagen12.jpg : ulleres: no , cabell: negre , sexe: dona";
+    $cosas = explode(' ',$entrada);
+    print_r($cosas);
      ?>
   </body>
 </html>
