7083cdd7583026c38f637ffbdf3e25c45309e98a
nilarrus <nilarrus69@gmail.com>
2018-10-24 18:14:21 +0200

    pruebas is death

--- pruebas/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("buton").addEventlistener("click",funcion);
-//prototipo mayusculas funciones minusculas
-function functionName() {
-  this.algo;
-  this.algo;
-}
-ClassName.prototype.methodName = function () {
-  //code
-};
